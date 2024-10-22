aa343cdedb021bbcaae8cc37b8e5890df73e9cbd
jianyuoo <108246265+jianyuoo@users.noreply.github.com>
2024-10-22 20:11:24 +0800

    框架结构修改

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <router-view/>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+/***************** CSS重置 *******************/
+html,body,div,span,h1,h2,h3,h4,h5,h6,ul,ol,li,p{
+  margin: 0;
+  padding: 0;
+}
+html,body,#app{
+  width: 100%;
+  height: 100%;
+  font-family: "微软雅黑";
+}
+ul,ol{
+  list-style:none;
+}
+a{
+  text-decoration: none;
 }
 </style>
